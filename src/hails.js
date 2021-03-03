f97a73a48c409d09f39637bc6ea604860d60c5ad
Julien Castets <castets.j@gmail.com>
2021-03-03 16:12:59 +0100

    Add src/hails.js to list hails

---
+++ src/hails.js
@@ -0,0 +1,22 @@
+import { request } from './api';
+
+export async function listHails(token, page, filters) {
+  const url = '/hails';
+  const args = {};
+
+  if (page) {
+    args.p = page + 1;
+  }
+  if (filters?.id) {
+    args.id = filters.id;
+  }
+  if (filters?.taxi_id) {
+    args.taxi_id = filters.taxi_id;
+  }
+
+  const resp = await request(url, {
+    token,
+    args,
+  });
+  return { hails: resp.data, meta: resp.meta };
+}
