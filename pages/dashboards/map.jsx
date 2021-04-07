f43b7c186063e25dc063e493e06867dcb20f233f
Julien Castets <castets.j@gmail.com>
2021-04-07 10:59:32 +0200

    Add missing, uncommitted file

---
+++ pages/dashboards/map.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import dynamic from 'next/dynamic';
+
+import { Layout } from './index';
+
+export default function DashboardMap() {
+  const Map = dynamic(
+    () => import('../../components/TaxisMap'),
+    { ssr: false },
+  );
+
+  return (
+    <Layout>
+      <Map />
+    </Layout>
+  );
+}
