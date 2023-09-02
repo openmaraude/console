d7beb7c76da70bd6661e3ae19df58b71dda4a8aa
Hervé Cauwelier <herve.cauwelier@beta.gouv.fr>
2023-09-02 16:53:43 +0200

    try and fix sentry update
    Sentry 7.65 was flawed, but BrowserTracing was obsolete nonetheless.

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -9,10 +9,8 @@ if (process.env.NODE_ENV === 'production') {
   const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
   Sentry.init({
-    integrations: [
-      new Integrations.BrowserTracing(),
-    ],
     dsn: SENTRY_DSN || 'https://3652397b192141b2a0e8e6a6678c41c7@o481095.ingest.sentry.io/5736209',
+    integrations: [new Sentry.Replay()],
     tracesSampleRate: 0.01,
   });
 }
